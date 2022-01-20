567dfcb05614fcc9fcafba7cc4096e1c837a67fa
Zahra Ganji <38253772+s-ganji@users.noreply.github.com>
2022-01-20 12:35:57 +0330

    Add files via upload

---
+++ src/filesharing/Constraints/Commands.java
@@ -0,0 +1,7 @@
+package filesharing.Constraints;
+
+public class Commands {
+    public static final String PtoP = "p2p";
+    public static final String RECEIVE = "-receive";
+    public static final String SERVE = "-serve";
+}
---
+++ src/filesharing/Constraints/Configuration.java
@@ -0,0 +1,10 @@
+package filesharing.Constraints;
+
+public class Configuration {
+    public static final int SENDER_PORT = 25900;
+    public static final int RECEIVER_PORT = 25901;
+    public static final String BROADCAST_ADDRESS = "255.255.255.255";
+    //static final String PATH_OF_SHARED = "./shared/";
+    public static final String RECEIVED_FILE_ADDRESS = "./received/";
+
+}
